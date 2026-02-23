a31478dbbc79f816fd95a7be8c92fbe0f2f956e3
Aman Raj <amanraj@Amans-MacBook-Air.local>
2026-02-23 12:07:46 +0530

    clenaup

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main (String[] args) {
-        for(int i=65; i<=70; i++){
-            System.out.println((char)i);
-        }
-
-    }
-}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HOT
 link[https://codeforces.com/group/4vcXCPx8NY/contests]
+
+## codeforces account
+## wakatime extension in vscode -> (wakatim.com)
